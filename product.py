8b8cb2559e718d41f4fd9fced9b7c7fc06333000
yuting1108 <sweet5845187@gmail.com>
2021-05-17 11:37:28 +0800

    add product

---
+++ product.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	p = []
+	p.append(name)
+	p.append(price)
+	products.append(p)
+print(products)
+
+products[0][0]
